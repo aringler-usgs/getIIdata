1cf1f92ab1314a9e9013f7820259afbcc995b880
MatthewKline <mr.kline30@gmail.com>
2014-07-21 11:55:44 -0600

    the for loop is definitely wrong

--- getIIdata.py
+++ getIIdata.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python
 
-###################################################################################################
-#This code pulls network data from IRIS and sets it up to be put in a directory structure
-#
-#
-#
-#
-###################################################################################################
-
-
-#TO DO: Add start and end day to parser
-#	Create directory structure to add to /TEST_ARCHIVE
-#	Finish adding wildcards
-#	If not adding the data to /TEST_ARCHIVE we should include the station
-
-
 import glob
 import urllib
 import os
@@ -33,248 +18,266 @@ from obspy.core.stream import Stream
 from obspy.core.utcdatetime import UTCDateTime
 from obspy.fdsn import Client
 
-#Need to specify which station and day we want to get data for
-parser = argparse.ArgumentParser(description='Code to get dataless from getIIdata.py')
+class GetIIData(object):
+	# initialize input vars
+	def __init__(self, year, startday, network, **kwargs):
+		# initialize year/start/net
+		# if statement to check for main args set QUERY=True	
+		# else sys.exit(1)
+		if (year != "") and (startday != "") and (network != ""):	
+			self.year = year
+			self.startday = startday
+			self.network = network
+			QUERY = True
+		else:
+			QUERY = False
 
-parser.add_argument('-y', action = "store",dest="year", \
-default = "*", help="Year of collected data: YYYY", type = str, required = True)
+		# loop through **kwargs and initialize optargs
+		self.endday = ""	# init endday string
+		self.station = "" 	# init station string
+		self.location = ""	# init location string
+		self.channel = ""	# init channel string
+		self.debug = False	# init debug
+		self.archive = False	# init archive
+		endday = self.endday
+		for key,val in kwargs.iteritems(): 
+			if key == "endday": self.endday = val
+			elif key == "station": self.station = val
+			elif key == "location": self.location = val
+			elif key == "channel": self.channel = val
+			elif key == "debug": self.debug = self.toBool(val)
+			elif key == "archive": self.archive = self.toBool(val) 
 
-parser.add_argument('-sd','--start day        ', action = "store",dest="startday", \
-default = "*", help="Day of collected data: DDD", type = str, required = True)
+		# print arguments if 'debug' mode
+		if self.debug:
+			print "Year: " + self.year
+			print "Start Day: " + self.startday
+			print "End Day: " + self.endday
+			print "Network: " + self.network
+			print "Station: " + self.station
+			print "Location: " + self.location
+			print "Channel: " + self.channel
 
-parser.add_argument('-ed','--end day        ', action = "store",dest="endday", \
-default = "*", help="Day of collected data: DDD", type = str, required = True)
+		# handle wildcards
+		if self.location == "?":
+			self.location = "*"
+		if self.channel == "?":
+			self.channel = "*"
+		if self.station == "?":
+			self.station = "*"
 
-parser.add_argument('-nslc', action = "store", dest= "nslc" , nargs = "+" , \
-help="Enter NN SSSSS LL CCC cannot wildcard network", type = str, required = True)
+		# set start/end to UTCDateTime object
+		#--------------------------------------------------------------------
+		self.startTime = UTCDateTime(year + startday +"T00:00:00.000")
+		#If no end day in parser default to 1 day
+		if self.endday == "?":
+			self.endTime = self.startTime + 24*60*60
+		else:
+			self.endTime = UTCDateTime(year + self.endday +"T00:00:00.000")
+		print "Here is our start time " + self.startTime.formatIRISWebService()
+		print "Here is our end time   " + self.endTime.formatIRISWebService()
+		# Will only run if main args are given
+		# check QUERY flag if True continue
+		if QUERY:
+			self.queryData()
+		else:
+			print '\nNo main args given.'
+			print 'Exiting\n'
+			sys.exit(1)
+	
+	def queryData(self):
+		# code from IRIS client 
+		#Here we pull the data
+		client = Client("IRIS")
+		DupStations = []
+		DupLocations = []
+		DupChannels = []
+		self.STAWILD = False
+		self.LOCWILD = False
+		self.CHANWILD = False
+		try:
+			requestArray = [(self.network,self.station,self.location, \
+				self.channel,self.startTime,self.endTime)]
+			print
+			if self.debug:
+				print(requestArray)
+				print 
+			self.st = client.get_waveforms_bulk(requestArray)
+			for self.tr in self.st:
+				#Here we remove the M data quality and go with D
+				self.tr.stats.mseed['dataquality'] = 'D'
+				if self.debug:
+					#print "Here is a trace we have"
+					#print(tr.stats)
+					if self.station == '*':
+						self.STAWILD = True
+						DupStations.append(self.tr.stats.station)				
+		    			elif self.station != '*':
+                				self.STAWILD = False
 
-parser.add_argument('-d','--debug',action = "store_true",dest="debug", \
-default = False, help="Run in debug mode")
-
-parser.add_argument('-a','--archive',action = "store_true",dest="archive", \
-default = False, help="Archive the data in /TEST_ARCHIVE")
-
-parserval = parser.parse_args()
-
-#Here is debug mode
-if parserval.debug:
-	debug = True
-else:
-	debug = False
-
-try:
-	year = parserval.year
-	startday = parserval.startday
-	endday = parserval.endday
-	net = parserval.nslc[0]
-	sta = parserval.nslc[1]
-	loc = parserval.nslc[2]
-	chan = parserval.nslc[3]
-	archive = parserval.archive
-	if debug:
-		print "Year: " + year
-		print "Start Day: " + startday
-		print "End Day: " + endday
-		print "Net: " + net
-		print "Station: " + sta
-		print "Location: " + loc
-		print "Channel: " + chan
-except:
-	print "Can not read in values"
-	sys.exit(0)
-
-#Here we reparse the wildcards
-
-if loc == "?":
-	loc = "*"
-if sta == "?":
-	sta = "*"
-if chan == "?":
-	chan = "*"
-if net == "?":
-	print "Wildcarding a network is not allowed"
-	sys.exit(0)
-
-#Here we set the day and year to a UTCDateTime object
-startTime = UTCDateTime(year + startday +"T00:00:00.000")
-#If no end day in parser default to 1 day
-if endday == "?":
-	endTime = startTime + 24*60*60
-else: 
-	endTime = UTCDateTime(year + endday +"T00:00:00.000")
-
-if debug:
-	print "Here is our start time " + startTime.formatIRISWebService()
-	print "Here is our end time   " + endTime.formatIRISWebService()
-	print 	
-
-#Here we pull the data
-client = Client("IRIS")
-DupStations = []
-DupLocations = []
-DupChannels = []
-STAWILD = False
-LOCWILD = False
-CHANWILD = False
-try:
-	requestArray = [(net,sta,loc,chan,startTime,endTime)]
-	if debug:
-		print(requestArray)
-		print 
-	st = client.get_waveforms_bulk(requestArray)
-#	print st
-#	sys.exit(0)
-	for tr in st:
-		#Here we remove the M data quality and go with D
-		tr.stats.mseed['dataquality'] = 'D'
-		if debug:
-			#print "Here is a trace we have"
-			#print(tr.stats)
-			if sta == '*':
-				STAWILD = True
-				DupStations.append(tr.stats.station)				
-		    	elif sta != '*':
-                		STAWILD = False
-
-            		if loc == '*':
-				LOCWILD = True	
-                		DupLocations.append(tr.stats.location)
-		    	elif loc != '*':
-				LOCWILD = False 
+            				if self.location == '*':
+						self.LOCWILD = True	
+                				DupLocations.append(self.tr.stats.location)
+		    			elif self.location != '*':
+						self.LOCWILD = False 
 			
-			if chan == '*':
-				CHANWILD = True	
-                		DupChannels.append(tr.stats.channel)
-		    	elif chan != '*':
-				CHANWILD = False 
-except:
-	print 'Trouble getting data'
-	sys.exit(0)
-#takes duplicate stations out of list
-stations = list(set(DupStations))
-locations = list(set(DupLocations))
-channels = list(set(DupChannels))
-print stations
-print locations
-print channels
-#One parser flag could be to the local directory the other could go to 
-#/TEST_ARCHIVE
-if archive:
-	if debug:
-		print "We are archiving the data to /TEST_ARCHIVE"
+					if self.channel == '*':
+						self.CHANWILD = True	
+                				DupChannels.append(self.tr.stats.channel)
+		    			elif self.channel != '*':
+						self.CHANWILD = False 
+		except:
+			print 'Trouble getting data'
+			sys.exit(0)
+		# Takes duplicate stations out of list and 
+		# makes station, location, and channel into an array 
+		# for looping( probably easier way but it works)
+		self.stations = list(set(DupStations))
+		self.stations.append(self.station)
+		self.locations = list(set(DupLocations))
+		self.locations.append(self.location)
+		self.channels = list(set(DupChannels))
+		self.channels.append(self.channel)
+			
+		# Now call code to store streams in mseed files
+		self.storeMSEED()
+	
+	def storeMSEED(self):
+		#code for storing MSEED files
+		#Need to check if the directories exist and if not make them
+		#Main program
+		codepath = '/home/mkline/dev/getIIdataBackup/TEST_ARCHIVE/'
+		self.days = int(round((self.st[-1].stats.endtime \
+			- self.st[0].stats.starttime)/(24*60*60)))
+		self.stFinal = Stream()
 
-if True:
-	if debug:
-		print "We are writing the data" 
-
-	#Need to check if the directories exist and if not make them
-	#Main program
-	codepath = '/home/mkline/dev/getIIdata/TEST_ARCHIVE/'
-	days = int(round((st[-1].stats.endtime - st[0].stats.starttime)/(24*60*60)))
-	stFinal = Stream()
-
-	if STAWILD:
-		for sta in stations:
-			trace = st.select(station = sta)
+		for self.station in self.stations:
+			print
+			print "For station: " + self.station
+			trace = self.st.select(station = self.station)
 			trace.merge()
 			trace.sort()
 			trace.count()
-			#Converting date into julian day
-			#this is here to make sure that all the traces start on the same day
-			timesplit = re.split('T', str(trace[0].stats.starttime))
-			s = timesplit[0]
-			fmt = '%Y-%m-%d'
-			dt = datetime.datetime.strptime(s, fmt)
-			tt = dt.timetuple()
-			if tt.tm_yday < 10:
-				NewStartDay = '00' + str(tt.tm_yday)
-			elif tt.tm_yday < 100:
-				NewStartDay = '0' + str(tt.tm_yday)
-			else:
-				NewStartDay = str(tt.tm_yday)
-			#index error if these dont match for the given station
-			if startday != NewStartDay :
-				stations.remove(sta)
-				print "This station doesnt have seed data for this day"
-			else:
-				for dayIndex in range(0,days):
-					print "Day properties: "
-					trimStart = trace[0].stats.starttime + (dayIndex)*24*60*60
-					trimEnd = trace[0].stats.starttime + (dayIndex+1)*24*60*60
-					print "Start of day: " + str(trimStart)
-					print "End of day:   " + str(trimEnd)
-					#Converting date into julian day
-					timesplit = re.split('T', str(trimStart))
-					s = timesplit[0]
-					fmt = '%Y-%m-%d'
-					dt = datetime.datetime.strptime(s, fmt)
-					tt = dt.timetuple()
-					if tt.tm_yday < 10:
-						NewStartDay = '00' + str(tt.tm_yday)
-					elif tt.tm_yday < 100:
-						NewStartDay = '0' + str(tt.tm_yday)
-					else:
-						NewStartDay = str(tt.tm_yday)
-					stFinal = trace.copy()
-					stFinal.trim(starttime = trimStart, endtime = trimEnd)	
-					stFinal = stFinal.split()
-					#Added the directory structures in here since you won't want to
-					#add directory structures that you don't use
-					if not os.path.exists(codepath + net + '_' + sta  + '/'):
-						os.mkdir(codepath + net + '_' + sta  + '/')
-					if not os.path.exists(codepath + net + '_' + sta  + '/' + year + '/'):
-						os.mkdir(codepath + net + '_' + sta  + '/' + year + '/')
-					stpath = codepath + net + '_' + sta  + '/' + year + '/' \
-						+ year + '_' + NewStartDay + '/'
-					if not os.path.exists(stpath):
-						os.mkdir(stpath)
-					# Here we write the data using STEIM 2 and 512 record lengths
-					stFinal.write(stpath + stFinal[0].stats.location + '_' + stFinal[0].stats.channel \
-						+ '.512.seed', format='MSEED',reclen = 512, encoding='STEIM2')
-					print stFinal
-					print
-    	elif LOCWILD: 
-		for loc in locations:
-			trace = st.select(location = loc)
-			trace.merge()
-			trace.sort()
-			trace.count()
-			print "For station: " + sta
-			for dayIndex in range(0,days):
-				print "Day properties: "
-				trimStart = trace[0].stats.starttime + (dayIndex)*24*60*60
-				trimEnd = trace[0].stats.starttime + (dayIndex+1)*24*60*60
-				print "Start of day: " + str(trimStart)
-				print "End of day:   " + str(trimEnd)
-				#Converting date into julian day
-				timesplit = re.split('T', str(trimStart))
-				s = timesplit[0]
-				fmt = '%Y-%m-%d'
-				dt = datetime.datetime.strptime(s, fmt)
-				tt = dt.timetuple()
-				if tt.tm_yday < 10:
-					NewStartDay = '00' + str(tt.tm_yday)
-				elif tt.tm_yday < 100:
-					NewStartDay = '0' + str(tt.tm_yday)
-				else:
-					NewStartDay = str(tt.tm_yday)
-				stFinal = trace.copy()
-				stFinal.trim(starttime = trimStart, endtime = trimEnd)	
-				stFinal = stFinal.split()
-				#Added the directory structures in here since you won't want to
-				#add directory structures that you don't use
-				#problem with this is the current loop does not run through all of the stations!
-				print
-				if not os.path.exists(codepath + net + '_' + sta  + '/'):
-					os.mkdir(codepath + net + '_' + sta  + '/')
-				if not os.path.exists(codepath + net + '_' + sta  + '/' + year + '/'):
-					os.mkdir(codepath + net + '_' + sta  + '/' + year + '/')
-				stpath = codepath + net + '_' + sta  + '/' + year + '/' \
-					+ year + '_' + NewStartDay + '/'
-				if not os.path.exists(stpath):
-					os.mkdir(stpath)
-				print
-				# Here we write the data using STEIM 2 and 512 record lengths
-				stFinal.write(stpath + stFinal[0].stats.location + '_' + stFinal[0].stats.channel \
-					+ '.512.seed', format='MSEED',reclen = 512, encoding='STEIM2')
-				print stFinal
+			for self.location in self.locations:
+				trace = self.st.select(location = self.location)
+				trace.merge()
+				trace.sort()
+				trace.count()
+				for self.channel in self.channels:
+					trace = self.st.select(channel = self.channel)
+					trace.merge()
+					trace.sort()
+					trace.count()
+					for dayIndex in range(0,self.days):
+						print "Day properties: "
+						#startTime works better than trace[0].stats.starttime
+						trimStart = self.startTime + (dayIndex)*24*60*60
+						trimEnd = self.startTime + (dayIndex+1)*24*60*60
+						print "Start of day: " + str(trimStart)
+						print "End of day:   " + str(trimEnd)
+						#Converting date into julian day
+						timesplit = re.split('T', str(trimStart))
+						s = timesplit[0]
+						fmt = '%Y-%m-%d'
+						dt = datetime.datetime.strptime(s, fmt)
+						tt = dt.timetuple()
+						if tt.tm_yday < 10:
+							NewStartDay = '00' + str(tt.tm_yday)
+						elif tt.tm_yday < 100:
+							NewStartDay = '0' + str(tt.tm_yday)
+						else:
+							NewStartDay = str(tt.tm_yday)
+						self.stFinal = trace.copy()
+						self.stFinal.trim(starttime = trimStart, endtime = trimEnd)	
+						self.stFinal = self.stFinal.split()
+						if not self.stFinal:
+							print "No trace for given day"
+						else:
+							#Added the directory structures in here since you won't want to
+							#add directory structures that you don't use
+							if not os.path.exists(codepath + self.network + '_' + self.station  + '/'):
+								os.mkdir(codepath + self.network + '_' + self.station  + '/')
+							if not os.path.exists(codepath + self.network + '_' + self.station  + '/' \
+								+ self.year + '/'):
+								os.mkdir(codepath + self.network + '_' + self.station  + '/' \
+								+ self.year + '/')
+							stpath = codepath + self.network + '_' + self.station  + '/' + self.year + \
+								'/' + self.year + '_' + NewStartDay + '/'
+							if not os.path.exists(stpath):
+								os.mkdir(stpath)
+							# Here we write the data using STEIM 2 and 512 record lengths
+							self.stFinal.write(stpath + self.stFinal[0].stats.location + '_' + \
+								self.stFinal[0].stats.channel + '.512.seed', format='MSEED', \
+								reclen = 512, encoding='STEIM2')
+							print self.stFinal
+							
+
+	# convert optional boolean strings to boolean vars
+	def toBool(self, value):
+		"""
+		Converts 'string' to boolean. Raises exception for invalid formats
+			True values: 1, True, true, "1", "True", "true", "yes", "y", "t"
+			False values: 0, False, false, "0", "False", "false", "no", "n", "f" 
+		"""
+		if str(value).lower() in ("true", "yes", "t", "y", "1"): return True
+		if str(value).lower() in ("false", "no", "f", "n", "0"): return False
+		raise Exception('Invalid value for boolean conversion: ' + str(value))
+
+# Subclass GetArgs() passes individual 
+# args to superclass GetIIData()
+class GetArgs(GetIIData):
+	def __init__(self, *args, **kwargs):
+		if not args:
+			print '\nPass in year, startday, network.'
+			print 'Exiting...\n'
+			sys.exit(1)
+
+		# store positional args
+		year = args[0]
+		startday = args[1]
+		network = args[2]
+
+		# pass *args and **kwargs to super() class
+		super(GetArgs, self).__init__(year, startday, network, **kwargs)
+
+def Help():
+	# help file that takes the same format as python parser
+	usage = """Usage: This code pulls network data from IRIS and sets 
+	it up to be put in a directory structure. The data that is pulled is 
+	from the II network given the year and start day that the data is from.
+	The end day is an optional arguement; if no end day specified, it will 
+	be one day after start day given. Station, location, and channel can be
+	wildcards or given a specific value. Only one of these can be wildcarded
+	at a time. If the station, location, and channel given do not return any 
+	data(trace(s)), IRIS did not collect data for that specific start day given.
+	Optionally the user can:
+		- specify an end day to collect data for multiple days
+		- choose a specific station, location, and/or channel 
+		- archive the data if it has yet to be stored in the directory structure
+	Arguments will be passed to:
+		- GetIIData.GetArgs(posargs,optargs)
+		- posargs - positional arguments (network, year, startday)
+		- optargs - optional arguments (endday, station, location, channel, debug, archive)
+	"""
+
+	posargs = """postional arguments:
+	(network = 'II')			\tnetwork in which data is pulled from
+	(year = 'YYYY')				\tyear of collected data
+	(startday = 'DDD')			\tthe left edge of time series (Julian Day)
+	"""
+	
+	optargs = """optional arguments:
+	(endday = 'DDD')			\tthe right edge of time series (Julian Day)
+	(station = 'SSSSS')			\tspecific station to where to pull data from
+						  (if all stations wanted, enter '?')
+	(location = 'LL')			\tspecific location to where to pull data from
+						  (if all locations wanted, enter '?')
+	(channel = 'CCC')			\tspecific channel to where to pull data from
+						  (if all channels wanted, enter '?')
+	(debug = 'True/False')			\trun in debug mode
+	(archive = 'True/False')		\tarchive the data in /TEST_ARCHIVE
+	"""
+
+	print usage
+	print posargs
+	print optargs
